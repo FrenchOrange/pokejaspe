3d0b1c432787629b063fd7787db942ec14c2fd70
FrenchOrange <64930003+FrenchOrange@users.noreply.github.com>
2024-03-01 05:20:03 +0100

    Repo setup

---
+++ maps/PlayersHouse2F.asm
@@ -0,0 +1,332 @@
+PlayersHouse2F_MapScriptHeader:
+	def_scene_scripts
+
+	def_callbacks
+	callback MAPCALLBACK_NEWMAP, PlayersHouse2FInitializeRoom
+	callback MAPCALLBACK_TILES, PlayersHouse2FSetSpawn
+
+	def_warp_events
+	warp_event  7,  0, PLAYERS_HOUSE_1F, 3
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  2,  1, BGEVENT_UP, PlayersHousePC
+	bg_event  3,  1, BGEVENT_READ, PlayersHouseRadio
+	bg_event  5,  1, BGEVENT_READ, PokemonJournalProfElmScript
+	bg_event  6,  0, BGEVENT_IFSET, PlayersHousePoster
+	bg_event  1,  0, BGEVENT_READ, DebugSign
+
+	def_object_events
+	object_event  4,  2, SPRITE_CONSOLE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GameConsole, EVENT_PLAYERS_HOUSE_2F_CONSOLE
+	object_event  4,  4, SPRITE_DOLL_1, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Doll1, EVENT_PLAYERS_HOUSE_2F_DOLL_1
+	object_event  5,  4, SPRITE_DOLL_2, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Doll2, EVENT_PLAYERS_HOUSE_2F_DOLL_2
+	object_event  0,  1, SPRITE_BIG_DOLL, SPRITEMOVEDATA_BIGDOLL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BigDoll, EVENT_PLAYERS_HOUSE_2F_BIG_DOLL
+
+PlayersHouse2FInitializeRoom:
+	special ToggleDecorationsVisibility
+	setevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_8
+	endcallback
+
+PlayersHouse2FSetSpawn:
+	special ToggleMaptileDecorations
+	endcallback
+
+Doll1:
+	describedecoration 1
+
+Doll2:
+	describedecoration 2
+
+BigDoll:
+	describedecoration 3
+
+GameConsole:
+	describedecoration 4
+
+PlayersHousePoster:
+	dw EVENT_PLAYERS_ROOM_POSTER
+	describedecoration 0
+
+PlayersHouseRadio:
+	checkevent EVENT_GOT_A_POKEMON_FROM_ELM
+	iftruefwd .NormalRadio
+	checkevent EVENT_LISTENED_TO_INITIAL_RADIO
+	iftruefwd .AbbreviatedRadio
+	playmusic MUSIC_POKEMON_TALK
+	opentext
+	writetext PlayerRadioText1
+	pause 45
+	writetext PlayerRadioText2
+	pause 45
+	writetext PlayerRadioText3
+	pause 45
+	musicfadeout MUSIC_NEW_BARK_TOWN, 16
+	writetext PlayerRadioText4
+	pause 45
+	closetext
+	setevent EVENT_LISTENED_TO_INITIAL_RADIO
+	end
+
+.NormalRadio:
+	jumpstd radio1
+
+.AbbreviatedRadio:
+	opentext
+	writetext PlayerRadioText4
+	pause 45
+	endtext
+
+DebugSign:
+	opentext
+	; time
+	special Special_SetDayOfWeek
+	special Special_InitialClearDSTFlag
+	; full pokegear
+	setflag ENGINE_POKEGEAR
+	setflag ENGINE_PHONE_CARD
+	setflag ENGINE_MAP_CARD
+	setflag ENGINE_RADIO_CARD
+	setflag ENGINE_EXPN_CARD
+	; pokedex
+	setflag ENGINE_POKEDEX
+;	setflag ENGINE_UNOWN_DEX
+	; judge machine
+	setflag ENGINE_JUDGE_MACHINE
+	; all key items
+for x, 1, NUM_KEY_ITEMS + 1
+if x != MACHINE_PART
+	givekeyitem x
+endc
+endr
+	; all tms+hms
+for x, NUM_TMS + NUM_HMS
+	givetmhm x
+endr
+	; useful items
+for x, POKE_BALL, CHERISH_BALL + 1
+if x != PARK_BALL && x != SAFARI_BALL
+	giveitem x, 99
+endc
+endr
+	giveitem MAX_POTION, 99
+	giveitem FULL_RESTORE, 99
+	giveitem MAX_REVIVE, 99
+	giveitem MAX_ELIXIR, 99
+	giveitem HP_UP, 99
+	giveitem PROTEIN, 99
+	giveitem IRON, 99
+	giveitem CARBOS, 99
+	giveitem CALCIUM, 99
+	giveitem ZINC, 99
+	giveitem RARE_CANDY, 99
+	giveitem PP_UP, 99
+	giveitem PP_MAX, 99
+	giveitem SACRED_ASH, 99
+	giveitem MAX_REPEL, 99
+	giveitem MAX_REPEL, 99
+	giveitem ESCAPE_ROPE, 99
+	giveitem ABILITY_CAP, 99
+	giveitem LEAF_STONE, 99
+	giveitem FIRE_STONE, 99
+	giveitem WATER_STONE, 99
+	giveitem THUNDERSTONE, 99
+	giveitem ICE_STONE, 99
+	giveitem MOON_STONE, 99
+	giveitem SUN_STONE, 99
+	giveitem DUSK_STONE, 99
+	giveitem SHINY_STONE, 99
+	giveitem ODD_SOUVENIR, 99
+	giveitem EXP_SHARE, 99
+	giveitem LEFTOVERS, 99
+	giveitem MULCH, 99
+	giveitem SWEET_HONEY, 99
+	giveitem SILVER_LEAF, 99
+	giveitem GOLD_LEAF, 99
+	giveitem MINT_LEAF, 99
+	giveitem BOTTLE_CAP, 99
+	giveitem BIG_NUGGET, 99
+	giveitem ARMOR_SUIT, 1
+	; all decorations except Diploma
+for x, EVENT_DECO_BED_1, EVENT_DECO_BIG_LAPRAS_DOLL + 1
+	setevent x
+endr
+	; max money
+	givemoney $0, 1000000
+	givemoney $0, 1000000
+	givemoney $0, 1000000
+	givemoney $0, 1000000
+	givemoney $0, 1000000
+	givemoney $0, 1000000
+	givemoney $0, 1000000
+	givemoney $0, 1000000
+	givemoney $0, 1000000
+	givemoney $0, 999999
+	givecoins 50000
+	loadmem wBattlePoints+0, 0
+	loadmem wBattlePoints+1, 250
+	; all badges
+	setflag ENGINE_ZEPHYRBADGE
+	setflag ENGINE_HIVEBADGE
+	setflag ENGINE_PLAINBADGE
+	setflag ENGINE_FOGBADGE
+	setflag ENGINE_STORMBADGE
+	setflag ENGINE_MINERALBADGE
+	setflag ENGINE_GLACIERBADGE
+	setflag ENGINE_RISINGBADGE
+	setflag ENGINE_BOULDERBADGE
+	setflag ENGINE_CASCADEBADGE
+	setflag ENGINE_THUNDERBADGE
+	setflag ENGINE_RAINBOWBADGE
+	setflag ENGINE_MARSHBADGE
+	setflag ENGINE_SOULBADGE
+	setflag ENGINE_VOLCANOBADGE
+	setflag ENGINE_EARTHBADGE
+	setevent EVENT_BEAT_FALKNER
+	setevent EVENT_BEAT_BUGSY
+	setevent EVENT_BEAT_WHITNEY
+	setevent EVENT_BEAT_MORTY
+	setevent EVENT_BEAT_CHUCK
+	setevent EVENT_BEAT_JASMINE
+	setevent EVENT_BEAT_PRYCE
+	setevent EVENT_BEAT_CLAIR
+	setevent EVENT_BEAT_BROCK
+	setevent EVENT_BEAT_MISTY
+	setevent EVENT_BEAT_LTSURGE
+	setevent EVENT_BEAT_ERIKA
+	setevent EVENT_BEAT_JANINE
+	setevent EVENT_BEAT_SABRINA
+	setevent EVENT_BEAT_BLAINE
+	setevent EVENT_BEAT_BLUE
+	setevent EVENT_BEAT_ELITE_FOUR
+	setevent EVENT_BEAT_ELITE_FOUR_AGAIN
+	setevent EVENT_BATTLE_TOWER_OPEN
+	clearevent EVENT_BATTLE_TOWER_CLOSED
+	; fly anywhere
+	setflag ENGINE_FLYPOINT_NEW_BARK
+	setflag ENGINE_FLYPOINT_CHERRYGROVE
+	setflag ENGINE_FLYPOINT_VIOLET
+	setflag ENGINE_FLYPOINT_UNION_CAVE
+	setflag ENGINE_FLYPOINT_AZALEA
+	setflag ENGINE_FLYPOINT_GOLDENROD
+	setflag ENGINE_FLYPOINT_ECRUTEAK
+	setflag ENGINE_FLYPOINT_SNOWTOP_MOUNTAIN
+	setflag ENGINE_FLYPOINT_OLIVINE
+	setflag ENGINE_FLYPOINT_CIANWOOD
+	setflag ENGINE_FLYPOINT_YELLOW_FOREST
+	setflag ENGINE_FLYPOINT_MAHOGANY
+	setflag ENGINE_FLYPOINT_LAKE_OF_RAGE
+	setflag ENGINE_FLYPOINT_BLACKTHORN
+	setflag ENGINE_FLYPOINT_SILVER_CAVE
+	setflag ENGINE_FLYPOINT_INDIGO_PLATEAU
+	setflag ENGINE_FLYPOINT_PALLET
+	setflag ENGINE_FLYPOINT_VIRIDIAN
+	setflag ENGINE_FLYPOINT_PEWTER
+	setflag ENGINE_FLYPOINT_MT_MOON
+	setflag ENGINE_FLYPOINT_CERULEAN
+	setflag ENGINE_FLYPOINT_CERULEAN_CAPE
+	setflag ENGINE_FLYPOINT_VERMILION
+	setflag ENGINE_FLYPOINT_CELADON
+	setflag ENGINE_FLYPOINT_ROCK_TUNNEL
+	setflag ENGINE_FLYPOINT_LAVENDER
+	setflag ENGINE_FLYPOINT_FUCHSIA
+	setflag ENGINE_FLYPOINT_SAFFRON
+	setflag ENGINE_FLYPOINT_CINNABAR
+	setflag ENGINE_FLYPOINT_SHAMOUTI
+	setflag ENGINE_FLYPOINT_VALENCIA
+	setflag ENGINE_FLYPOINT_NAVEL
+	; magnet train works
+	setevent EVENT_RESTORED_POWER_TO_KANTO
+	; post-e4
+	setflag ENGINE_CREDITS_SKIP
+	; good party
+	givepoke MEWTWO, PLAIN_FORM, 100, BRIGHTPOWDER
+	; hm slave
+	givepoke MEW, PLAIN_FORM, 100, LEFTOVERS
+	loadmem wPartyMon2Moves+0, FLY
+	loadmem wPartyMon2Moves+1, HEADBUTT
+	loadmem wPartyMon2Moves+2, DIG
+	loadmem wPartyMon2Moves+3, FRESH_SNACK
+	loadmem wPartyMon2PP+0, 15
+	loadmem wPartyMon2PP+1, 15
+	loadmem wPartyMon2PP+2, 10
+	loadmem wPartyMon2PP+3, 10
+	; extra pokemon
+	givepoke FERALIGATR, PLAIN_FORM, 100
+	givepoke PIDGEOT, PLAIN_FORM, 100
+	givepoke LAPRAS, PLAIN_FORM, 100
+	givepoke PIKACHU, PLAIN_FORM, 100
+	; fill pokedex
+	callasm FillPokedex
+	setevent EVENT_GOT_RUNNING_SHOES
+	addcellnum PHONE_MOM
+	setmapscene PLAYERS_HOUSE_1F, $1
+	setevent EVENT_PLAYERS_HOUSE_MOM_1
+	clearevent EVENT_PLAYERS_HOUSE_MOM_2
+	addcellnum PHONE_ELM
+	closetext
+	playsound SFX_WARP_TO
+	special FadeOutPalettes
+	waitsfx
+	warpfacing DOWN, NEW_BARK_TOWN, 15, 6
+	end
+
+FillPokedex:
+	ld hl, wPokedexSeen
+	call .Fill
+	ld hl, wPokedexCaught
+.Fill:
+	ld a, %11111111
+	ld bc, NUM_UNIQUE_POKEMON / 8
+	rst ByteFill
+if NUM_UNIQUE_POKEMON % 8
+	ld [hl], 2**(NUM_UNIQUE_POKEMON % 8) - 1
+endc
+	ret
+
+PokemonJournalProfElmScript:
+	setflag ENGINE_READ_PROF_ELM_JOURNAL
+	jumpthistext
+
+	text "#mon Journal"
+
+	para "Special Feature:"
+	line "#mon Prof.Elm!"
+
+	para "Prof.Elm was the"
+	line "top student of"
+	cont "Prof.Oak."
+
+	para "They're said to"
+	line "often argue about"
+	cont "research."
+	done
+
+PlayersHousePC:
+	opentext
+	special Special_PlayersHousePC
+	iftruefwd .Warp
+	endtext
+.Warp:
+	warp NONE, 0, 0
+	end
+
+PlayerRadioText1:
+	text "Prof.Oak's #mon"
+	line "Talk! Please tune"
+	cont "in next time!"
+	done
+
+PlayerRadioText2:
+	text "#mon Channel!"
+	done
+
+PlayerRadioText3:
+	text "This is DJ Mary,"
+	line "your co-host!"
+	done
+
+PlayerRadioText4:
+	text "#mon!"
+	line "#mon Channel…"
+	done
